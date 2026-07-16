612ca6ad80e2274c8fa8efb4c356372ffb54c0a6
ramseywise <ramsey.wise@gmail.com>
2026-07-17 01:20:13 +0200

    add research bench resource

---
+++ generative-ai/chatbot/deep_research_bench/utils/io_utils.py
@@ -0,0 +1,9 @@
+import json
+
+
+def load_jsonl(file_path):
+    data = []
+    with open(file_path, "r") as f:
+        for line in f.readlines():
+            data.append(json.loads(line.strip()))
+    return data
